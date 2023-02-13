0fbde5064bc41ca7a7babd2c4594a3eddc137e78
shamengxin <1921028746@qq.com>
2023-02-13 11:56:02 +0800

    2-13

---
+++ spring6/spring6-homework019/src/main/java/com/shamengxin/spring6/bean/LogBeanPostProcessor.java
@@ -0,0 +1,18 @@
+package com.shamengxin.spring6.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class LogBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Bean后处理器before方法执行，即将开始初始化");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Bean后处理器after方法执行，初始化以完成");
+        return bean;
+    }
+}
